a62696657865d13aa722b207169f2781ba996f9a
NikolaiKryshnev <63440682+NikolaiKryshnev@users.noreply.github.com>
2021-03-11 10:10:34 +0300

    qweqw

--- mac/mac/js/script.js
+++ mac/mac/js/script.js
@@ -141,29 +141,29 @@ function burger(btn, block) {
 
 burger($('.burger-js'), $('.header-menu--js'))
 showModla($('.modal-overlay, .modal--form'), $('.popupBtn-js'));
-		function come(elem) {
-	var docViewTop = $(window).scrollTop(),
-		docViewBottom = docViewTop + $(window).height(),
-		elemTop = $(elem).offset().top,
-		elemBottom = elemTop + $(elem).height();
-	$(elem).addClass('animation')
-	return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
-}
+		// function come(elem) {
+// 	var docViewTop = $(window).scrollTop(),
+// 		docViewBottom = docViewTop + $(window).height(),
+// 		elemTop = $(elem).offset().top,
+// 		elemBottom = elemTop + $(elem).height();
+// 	$(elem).addClass('animation')
+// 	return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+// }
 
 
-let anBorder = $('.border')
-$(window).scroll(function () {
-	for (let i = 0; i < anBorder.length; i++) {
-		const el = anBorder[i];
-		if (come($(el))) {
-			console.log('yes');
-		} else {
-			console.log('no');
-			$(el).removeClass('animation')
-		}
-	}
+// let anBorder = $('.border')
+// $(window).scroll(function () {
+// 	for (let i = 0; i < anBorder.length; i++) {
+// 		const el = anBorder[i];
+// 		if (come($(el))) {
+// 			console.log('yes');
+// 		} else {
+// 			console.log('no');
+// 			$(el).removeClass('animation')
+// 		}
+// 	}
 
-});
+// });
 
 		//!smooth scroll top
 //---------------------------------------------------------------------------------------------------------------------------------------
