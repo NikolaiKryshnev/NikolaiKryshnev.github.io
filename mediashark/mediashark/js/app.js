51c29f21d7027e0db751585c201c2aeb2bc6c257
NikolaiKryshnev <63440682+NikolaiKryshnev@users.noreply.github.com>
2021-07-28 13:31:53 +0300

    feat: add animation

--- mediashark/mediashark/js/app.js
+++ mediashark/mediashark/js/app.js
@@ -1382,20 +1382,33 @@ anBtn()
 
 function footerFixed() {
 	let footer = document.querySelector('.footer'),
-	main = document.querySelector('.main');
+		main = document.querySelector('.main');
 
 	main.style.marginBottom = `${footer.offsetHeight}px`
 }
 footerFixed()
 
-window.onscroll = function() {myFunction()};
-
 function myFunction() {
-  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 100;
-//   document.querySelector(".panel-progres__proc").style.height = scrolled + "%";
+	let scrollBlock = document.querySelectorAll('._scroll-block'),
+		height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+	for (let i = 0; i < scrollBlock.length; i++) {
+		const el = scrollBlock[i];
+		let winScroll = el.scrollTop || document.documentElement.scrollTop,
+			scrolled = (winScroll / height) * 100,
+			topBlock = (el.offsetHeight + el.getBoundingClientRect().top) / 2;
+
+		// if ((el.getBoundingClientRect().top < 0)) {
+			if (Math.abs(el.getBoundingClientRect().top) <= el.offsetHeight)
+				console.log(`${el.offsetHeight}  + ${el.getBoundingClientRect().top} = ${el.offsetHeight + el.getBoundingClientRect().top}`);
+			el.querySelector('._scroll-text').style.top = `${topBlock}px`;
+		// }
+	}
 }
+
+myFunction()
+window.onscroll = function () { myFunction() };
+
 
 
 
